90ca72773a02ba7c6d8f9712513d8c7b6a281fe6
Jean <jean13franca@gmail.com>
2022-11-26 18:17:18 -0300

    feat/finishedSignIn

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,14 +1,18 @@
 import bcrypt from 'bcrypt';
-import { newUser } from '../interfaces/newUser';
+import { user } from '../interfaces/user';
+import { v4 as uuid } from 'uuid';
 import pkg from "@prisma/client";
+
 import AccountDuplicated from '../errors/AccountDuplicated';
+import EmaildDoesNotExist from '../errors/EmaildDoesNotExist';
+import WrongEmailOrPassword from '../errors/WrongEmailOrPassword';
 
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
 
-async function signUp({email,password}:newUser) {
-  const hash = bcrypt.hashSync(password, 10);
+async function signUp({email,password}:user) {
+  const hash = bcrypt.hashSync(password, 8);
   const user = await prisma.user.findUnique({
     where: { email }
   })
@@ -18,9 +22,30 @@ async function signUp({email,password}:newUser) {
   await prisma.user.create({
     data: {
       email: email,
-      password: password
+      password: hash
     },
   })
 }
 
-export {signUp}
+async function signIn({email,password}:user) {
+
+  const user = await prisma.user.findUnique({
+    where: { email }
+  })
+
+  console.log(user)
+
+  if (!user)  throw new EmaildDoesNotExist("This e-mail does not exist");
+  if (user && !bcrypt.compareSync(password, user.password))  throw new WrongEmailOrPassword("e-mail or password is invalid");
+
+  const token = uuid();
+
+  await prisma.session.create({
+    data: {
+      token: token,
+      userId: user.id
+    },
+  })
+}
+
+export {signUp, signIn}
