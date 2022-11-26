f631e4e4827decf4fd8fcc787f0333e8459fdd6e
Jean <jean13franca@gmail.com>
2022-11-26 15:43:03 -0300

    created validateToken

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import cors from "cors";
 import express from "express";
 
 import errorHandler from "./middlewares/errorHandler";
+import validateToken from "./middlewares/validateToken";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/health", async (req, res) => res.sendStatus(200));
+app.get("/health",validateToken, async (req, res) => await res.sendStatus(200));
 
 app.use(errorHandler);
 
