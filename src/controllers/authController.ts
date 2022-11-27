05c14204f1b7e4c58c1125f126fa5883cbc63224
Jean <jean13franca@gmail.com>
2022-11-27 12:12:12 -0300

    fix/return token at sign-in

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -23,8 +23,8 @@ async function signIn(req: Request, res: Response, next: NextFunction) {
 
   try {
     const validation = await validateUser(user);
-    await authService.signIn(user)
-    res.sendStatus(200);
+    const token = await authService.signIn(user)
+    res.send(token);
   } catch (error: any) {
     if (error.name === "ValidationError") return res.status(400).send(error.message)
     if (error.name === "EmaildDoesNotExist") return res.status(404).send(error.message)
